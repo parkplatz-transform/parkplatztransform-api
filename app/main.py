d3facc2cb8a0f4bdf0edd1d70e17dabd2918688d
Theo Lampert <mail@theo.sh>
2020-11-28 14:34:58 +0100

    🔧 Allow CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
-
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import segments, users
 
 app = FastAPI()
+
+origins = [
+    "https://pt.moewencloud.de/",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.include_router(users.router)
 app.include_router(segments.router)
