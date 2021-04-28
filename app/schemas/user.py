8c2033d1c1a5d7d87f49c6ffc9dc4a8a0fd70484
theolampert <4714866+theolampert@users.noreply.github.com>
2021-04-28 13:12:52 +0200

    chore: block any request without acceptance

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, StrictBool, validator
 from app.models.user import access_levels
 
 
 class UserBase(BaseModel):
     email: EmailStr
+    accepted_terms_and_conditions: StrictBool
+
+    @validator("accepted_terms_and_conditions")
+    def must_accept_terms(cls, v):
+        if not v:
+            raise ValueError("User must accept terms and conditions")
+        return v
 
     class Config:
         orm_mode = True
