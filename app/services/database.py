46795b6144a103f7dec36cfa06f21110e7961aef
theolampert <4714866+theolampert@users.noreply.github.com>
2021-05-04 14:53:18 +0200

    chore: start sqlalchemy upgrades

--- app/services/database.py
+++ app/services/database.py
@@ -5,7 +5,7 @@ from app.config import settings
 
 DATABASE_URL = settings.database_url
 
-engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0)
+engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=0, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
